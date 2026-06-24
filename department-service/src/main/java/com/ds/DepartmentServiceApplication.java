80e10f6a7f671ee81ecb75e0d6c085650c906097
Posa Karthik <posakarthik16@gmail.com>
2026-06-24 13:18:37 +0530

    Registered Department Service with Eureka Server

--- department-service/src/main/java/com/ds/DepartmentServiceApplication.java
+++ department-service/src/main/java/com/ds/DepartmentServiceApplication.java
@@ -2,8 +2,10 @@ package com.ds;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient  //in modern sbv optional
 public class DepartmentServiceApplication {
 
 	public static void main(String[] args) {
